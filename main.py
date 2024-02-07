fec2e1d417b7fd2d924f5e7d96519842c468b8b4
Lees <msle237.lees@gmail.com>
2024-02-07 11:38:54 -0500

    fixing cameras dun dun dun

--- main.py
+++ main.py
@@ -70,7 +70,7 @@ class CameraPackage:
             if not self.running:
                 try:
                     cam_pipeline_str = f'nvarguscamerasrc sensor-id={str(self.camera_index)} ! video/x-raw(memory:NVMM),format=NV12,width=1280,height=720,framerate=30/1 ! nvvidconv ! video/x-raw,format=BGRx ! videoconvert ! video/x-raw,format=BGR ! appsink drop=1'
-                    self.cap = cv2.VideoCapture(cam_pipeline_str + cv2.CAP_GSTREAMER)
+                    self.cap = cv2.VideoCapture(cam_pipeline_str, cv2.CAP_GSTREAMER)
                     self.running = True
 
                     self.camera_logger.info(f'Camera {self.camera_index} started')
